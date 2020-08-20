04c5c4ae3feacaf44d266396cc04f2c8ca8c0874
sreeragpavithran <46649210+sreeragpavithran@users.noreply.github.com>
2020-08-20 10:53:33 +0530

    Updates

---
+++ precache-manifest.6365e4cf78173940c0e4ea83a3c398d7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dbaaf25181b79ecc51cdcfddcc65f7e0",
+    "url": "/react-todo/index.html"
+  },
+  {
+    "revision": "807a86ef13cac4ba7375",
+    "url": "/react-todo/static/css/main.99577d7a.chunk.css"
+  },
+  {
+    "revision": "d83523455d1a57dd7ca9",
+    "url": "/react-todo/static/js/2.ea801fec.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-todo/static/js/2.ea801fec.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "807a86ef13cac4ba7375",
+    "url": "/react-todo/static/js/main.26acb8fa.chunk.js"
+  },
+  {
+    "revision": "c1536632fea85d80fba8",
+    "url": "/react-todo/static/js/runtime-main.09e4660a.js"
+  }
+]);
